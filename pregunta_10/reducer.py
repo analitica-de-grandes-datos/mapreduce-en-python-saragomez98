8dc0b144f2696358e82d501d62a6591d7a69cbec
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-29 19:49:26 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+letters_dict = {}
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        line_num, letters = line.split("\t")
+        line_num = int(line_num)
+        each_letter = letters.split(",")
+
+        for letter in each_letter:
+
+            letter = letter.strip()
+            if letter not in letters_dict.keys():
+                letters_dict[letter] = [line_num]
+
+            else:
+                letters_dict[letter] += [line_num]
+
+    for key in sorted(letters_dict.keys()):
+        str_nums = ",".join([str(i) for i in sorted(letters_dict[key])])
+        sys.stdout.write(
+            "{}\t{}\n".format(key, str_nums))
+    
