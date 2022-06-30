ba684b658f97ca123d7741de52578c780947bca8
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-29 19:52:50 -0500

    Update mapper.py

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+import itertools 
+
+if __name__ == '__main__':
+    
+ for line in sys.stdin:
+       letra=line.split('   ')[0] 
+       numero = int(line.split('   ')[2]) 
+       sys.stdout.write("{}\t{}\t1\n".format(letra,numero)) 
