8a081db13e0c87829357af13f5cd46abf64dce37
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-29 19:48:54 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+from operator import itemgetter
+import sys 
+if __name__ == "__main__":
+    for line in sys.stdin:
+        column_separated = line.split()
+
+        sys.stdout.write("{}\t{}\n".format(
+            column_separated[0], column_separated[1]))
